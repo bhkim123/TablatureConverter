7240909a1af15b114f2e51748dae47493058af61
bhkim123 <82852354+bhkim123@users.noreply.github.com>
2021-05-17 23:11:57 -0400

    converting and scripting

--- src/Converter/Measures.java
+++ src/Converter/Measures.java
@@ -21,8 +21,13 @@ public class Measures {
 
         for(Notes o : eachMeasrueNotes){
             ArrayList<String> temp = o.getCompletedNoteScript();
-            for(String script : temp){
-                scriptsPerMeasrue.add(script);
+            for(int i = 0; i < temp.size(); i++){
+                String noteScript = temp.get(i);
+                if(i == 0){
+                    addAttribute(noteScript);
+                }
+                addMeasureNum(noteScript, i + 1);
+                scriptsPerMeasrue.add(noteScript);
             }
         }
 
@@ -71,5 +76,53 @@ public class Measures {
             Notes notes = new Notes(eachMeasure);
             this.eachMeasrueNotes.add(notes);
         }
+    }
+
+    private void addAttribute(String noteScript){
+        noteScript = "<attributes>\n" +
+                "<divisions>2</divisions>\n" +
+                "<key>\n" +
+                "<fifths>0</fifths>\n" +
+                "</key>\n" +
+                "<time>\n" +
+                "<beats>4</beats>\n" +
+                "<beat-type>4</beat-type>\n" +
+                "</time>\n" +
+                "<clef>\n" +
+                "<sign>TAB</sign>\n" +
+                "<line>5</line>\n" +
+                "</clef>\n" +
+                "<staff-details>\n" +
+                "<staff-lines>6</staff-lines>\n" +
+                "<staff-tuning line=\"1\">\n" +
+                "<tuning-step>E</tuning-step>\n" +
+                "<tuning-octave>2</tuning-octave>\n" +
+                "</staff-tuning>\n" +
+                "<staff-tuning line=\"2\">\n" +
+                "<tuning-step>A</tuning-step>\n" +
+                "<tuning-octave>2</tuning-octave>\n" +
+                "</staff-tuning>\n" +
+                "<staff-tuning line=\"3\">\n" +
+                "<tuning-step>D</tuning-step>\n" +
+                "<tuning-octave>3</tuning-octave>\n" +
+                "</staff-tuning>\n" +
+                "<staff-tuning line=\"4\">\n" +
+                "<tuning-step>G</tuning-step>\n" +
+                "<tuning-octave>3</tuning-octave>\n" +
+                "</staff-tuning>\n" +
+                "<staff-tuning line=\"5\">\n" +
+                "<tuning-step>B</tuning-step>\n" +
+                "<tuning-octave>3</tuning-octave>\n" +
+                "</staff-tuning>\n" +
+                "<staff-tuning line=\"6\">\n" +
+                "<tuning-step>E</tuning-step>\n" +
+                "<tuning-octave>4</tuning-octave>\n" +
+                "</staff-tuning>\n" +
+                "</staff-details>\n" +
+                "</attributes>\n" + noteScript;
+    }
+
+    private void addMeasureNum(String noteScript, int measureNum){
+        noteScript = "<measure number=\"" + measureNum + "\">\n" + noteScript + "</measure>\n";
     }
 }
